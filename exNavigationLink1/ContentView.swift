e9781292ab7c40fcfeb2be393079334e2b11747b
Benedito Alberto Macedo <benedito.macedo@gmail.com>
2020-12-12 23:14:45 -0300

    display mode options

--- exNavigationLink1/ContentView.swift
+++ exNavigationLink1/ContentView.swift
@@ -4,6 +4,8 @@
 //
 //  Created by Benedito Macedo on 12/12/20.
 //
+// https://www.hackingwithswift.com/articles/216/complete-guide-to-navigationview-in-swiftui
+//
 
 import SwiftUI
 
@@ -15,7 +17,9 @@ struct ContentView: View {
                 label: {
                     /*@START_MENU_TOKEN@*/Text("Navigate")/*@END_MENU_TOKEN@*/
                 })
-                .navigationBarTitle("Navigation 2")
+//                .navigationBarTitle("Navigation 2")
+//                .navigationBarTitle("Navigation 2", displayMode: .large)
+                .navigationBarTitle("Navigation 2", displayMode: .inline)
         }
     }
 }
