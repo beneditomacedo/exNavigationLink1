65a8e9c204837cd47d7b4e323e7532e43865de2a
Benedito Alberto Macedo <benedito.macedo@gmail.com>
2020-12-12 23:11:16 -0300

    Inittial commit

--- exNavigationLink1/ContentView.swift
+++ exNavigationLink1/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            NavigationLink(
+                destination: /*@START_MENU_TOKEN@*/Text("Destination")/*@END_MENU_TOKEN@*/,
+                label: {
+                    /*@START_MENU_TOKEN@*/Text("Navigate")/*@END_MENU_TOKEN@*/
+                })
+                .navigationBarTitle("Navigation 2")
+        }
     }
 }
 
